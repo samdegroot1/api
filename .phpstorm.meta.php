36c8e72cce022a93f398351a7b70dc693f8109f4
Sam de Groot <s.degroot@nobears.nl>
2019-04-05 12:29:15 +0200

    phpstorm meta for autocomplete

---
+++ .phpstorm.meta.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PHPStorm addon to give our Core_Model factory usages autocompletion. See:
+ * https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Advanced+Metadata
+ */
+namespace PHPSTORM_META
+{
+    $STATIC_METHOD_TYPES = [
+        \Core\Model\CoreModel::factory('') => [
+            "" == "\App\Model\@"
+        ]
+    ];
+}
